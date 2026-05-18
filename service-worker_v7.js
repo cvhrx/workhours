2998dd4f77981cf2e48ad1ac02d42b225814b7a9
cvhrx <chrismendoza764@gmail.com>
2026-05-18 23:11:01 +0200

    Rename service-worker_v6.js to service-worker_v7.js

--- service-worker_v7.js
+++ service-worker_v7.js
@@ -1,3 +1,3 @@
-const CACHE_NAME='workhours-v6';
+const CACHE_NAME='workhours-v7';
 self.addEventListener('install',e=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(clients.claim()));
