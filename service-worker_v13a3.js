9f31c6a943fd1995ecd107e62933c608cb048eda
cvhrx <chrismendoza764@gmail.com>
2026-06-27 13:17:15 +0200

    Add files via upload

---
+++ service-worker_v13a3.js
@@ -0,0 +1,4 @@
+const CACHE='workhours-v13a3';
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE && k.startsWith('workhours-')).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{});
