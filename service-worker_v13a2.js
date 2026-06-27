c4e26efa4f9722dccb78b3448db94d0c5567f252
cvhrx <chrismendoza764@gmail.com>
2026-06-27 13:30:26 +0200

    Delete service-worker_v13a2.js

--- service-worker_v13a2.js
+++
@@ -1,4 +0,0 @@
-const CACHE='workhours-v13a2';
-self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE && k.startsWith('workhours-')).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{});
